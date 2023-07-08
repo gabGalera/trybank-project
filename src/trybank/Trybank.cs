f72b2804b73ed7cf99618018f8c2ef716088385f
Gabriel Galera <gabgalera@hotmail.com>
2023-07-08 16:38:57 -0300

    req1

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -2,65 +2,80 @@
 
 public class Trybank
 {
-    public bool Logged;
-    public int loggedUser;
-    
-    //0 -> Número da conta
-    //1 -> Agência
-    //2 -> Senha
-    //3 -> Saldo
-    public int[,] Bank;
-    public int registeredAccounts;
-    private int maxAccounts = 50;
-    public Trybank()
+  public bool Logged;
+  public int loggedUser;
+
+  //0 -> Número da conta
+  //1 -> Agência
+  //2 -> Senha
+  //3 -> Saldo
+  public int[,] Bank;
+  public int registeredAccounts = 0;
+  private int maxAccounts = 50;
+  public Trybank()
+  {
+    loggedUser = -99;
+    registeredAccounts = 0;
+    Logged = false;
+    Bank = new int[maxAccounts, 4];
+  }
+
+  // 1. Construa a funcionalidade de cadastrar novas contas
+  public void RegisterAccount(int number, int agency, int pass)
+  {
+    for (int i = 0; i < registeredAccounts; i++)
     {
-        loggedUser = -99;
-        registeredAccounts = 0;
-        Logged = false;
-        Bank = new int[maxAccounts, 4];
+      if (Bank[i, 0] == number || Bank[i, 1] == agency)
+      {
+        throw new ArgumentException("A conta já está sendo usada!");
+      }
     }
 
-    // 1. Construa a funcionalidade de cadastrar novas contas
-    public void RegisterAccount(int number, int agency, int pass)
-    {
-        throw new NotImplementedException();
-    }
+    int[] payload = { number, agency, pass, 0 };
 
-    // 2. Construa a funcionalidade de fazer Login
-    public void Login(int number, int agency, int pass)
+    for (int j = 0; j < 4; j++)
     {
-        throw new NotImplementedException();
+      Bank[registeredAccounts, j] = payload[j];
     }
+    registeredAccounts++;
+  }
 
-    // 3. Construa a funcionalidade de fazer Logout
-    public void Logout()
-    {
-        throw new NotImplementedException();
-    }
 
-    // 4. Construa a funcionalidade de checar o saldo
-    public int CheckBalance()
-    {
-        throw new NotImplementedException();   
-    }
+  // 2. Construa a funcionalidade de fazer Login
+  public void Login(int number, int agency, int pass)
+  {
+    throw new NotImplementedException();
+  }
 
-    // 5. Construa a funcionalidade de depositar dinheiro
-    public void Deposit(int value)
-    {
-        throw new NotImplementedException();
-    }
+  // 3. Construa a funcionalidade de fazer Logout
+  public void Logout()
+  {
+    throw new NotImplementedException();
+  }
 
-    // 6. Construa a funcionalidade de sacar dinheiro
-    public void Withdraw(int value)
-    {
-        throw new NotImplementedException();
-    }
+  // 4. Construa a funcionalidade de checar o saldo
+  public int CheckBalance()
+  {
+    throw new NotImplementedException();
+  }
 
-    // 7. Construa a funcionalidade de transferir dinheiro entre contas
-    public void Transfer(int destinationNumber, int destinationAgency, int value)
-    {
-        throw new NotImplementedException();
-    }
+  // 5. Construa a funcionalidade de depositar dinheiro
+  public void Deposit(int value)
+  {
+    throw new NotImplementedException();
+  }
 
-   
+  // 6. Construa a funcionalidade de sacar dinheiro
+  public void Withdraw(int value)
+  {
+    throw new NotImplementedException();
+  }
+
+  // 7. Construa a funcionalidade de transferir dinheiro entre contas
+  public void Transfer(int destinationNumber, int destinationAgency, int value)
+  {
+    throw new NotImplementedException();
+  }
+
+
 }
