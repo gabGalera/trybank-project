9cd70d36551bcb90ee29e9bfa5cb147507dd3a75
Gabriel Galera <gabgalera@hotmail.com>
2023-07-08 16:54:44 -0300

    req2

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -44,7 +44,34 @@ public class Trybank
   // 2. Construa a funcionalidade de fazer Login
   public void Login(int number, int agency, int pass)
   {
-    throw new NotImplementedException();
+    if (Logged)
+    {
+      throw new AccessViolationException("Usuário já está logado");
+    }
+    else
+    {
+      bool agencyFound = false;
+      for (int i = 0; i < registeredAccounts; i++)
+      {
+        if (Bank[i, 0] == number && Bank[i, 1] == agency)
+        {
+          agencyFound = true;
+          if (Bank[i, 2] == pass)
+          {
+            Logged = true;
+            loggedUser = i;
+          }
+          else
+          {
+            throw new ArgumentException("Senha incorreta");
+          }
+        }
+      }
+      if (agencyFound != true)
+      {
+        throw new ArgumentException("Agência + Conta não encontrada");
+      }
+    }
   }
 
   // 3. Construa a funcionalidade de fazer Logout
